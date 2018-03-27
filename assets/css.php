8ebb407c1cf34cc66c99663f6f654651138c64fd
Rai Icasiano <ricasiano@gmail.com>
2018-03-27 13:22:50 +0800

    moved to static

--- assets/css.php
+++
@@ -1,6 +0,0 @@
-<link href="dist/bitglip.min.css" rel="stylesheet">
-
-<!-- Custom Fonts -->
-<link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Orbitron' rel='stylesheet' type='text/css'>
--- config/default.php
+++
@@ -1,6 +0,0 @@
-<?php
-//SEO
-define('META_TITLE', 'BITGLIP | Your Startup Solution');
-define('META_AUTHOR', 'Rai Icasiano');
-define('META_DESCRIPTION', 'Your answer to your startup needs.');
-define('META_KEYWORDS', 'web, development, hosting, voip, startup, website, sms, android, ios, web development, web hosting');
